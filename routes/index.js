3bf56a37958860c1a006f54a97bba3b2f771bb30
jhonnpc04 <jhonnpc04>
2016-11-29 19:57:52 -0400

    start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var reclamo = {tiporeclamo:"odeco",observacion:"malservicio",fechareclamo:"29/11/2016"};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Avances en computacion' });
+  res.render('index', { title: "Proyecto final", subtitle:"avances en computacion",reclamo:reclamo});
 });
-
 module.exports = router;
